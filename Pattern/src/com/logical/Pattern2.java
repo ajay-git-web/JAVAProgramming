9c7a0ec4057fa1794762b71fdba872d4fab74f72
ajay.b <ajaybhosale852@gmail.com>
2023-10-16 22:24:09 +0530

    pattern2 added

--- Pattern/src/com/logical/Pattern2.java
+++ Pattern/src/com/logical/Pattern2.java
@@ -2,19 +2,18 @@ package com.logical;
 
 import java.util.Scanner;
 
-public class Pattern1 {
+public class Pattern2 {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter number of lines u want: ");
+		System.out.println("Enter a number u want lines for pattern");
 		int number=sc.nextInt();
-		
 		for(int i=1;i<=number;i++) {
-			for(int j=1;j<=number;j++) {
+			for(int j=1;j<=i;j++) {
 				System.out.print("*");
 			}System.out.println();
 		}
-	        
 	}
 
 }
