f01a06595009627cd22c9819bc4294ebe150e20a
ajay.b <ajaybhosale852@gmail.com>
2023-10-18 23:02:25 +0530

    branch commit Triangle star pyramid added

---
+++ Pattern/src/com/gfg/TriangleStarPattern.java
@@ -0,0 +1,36 @@
+package com.gfg;
+
+import java.util.Scanner;
+
+public class TriangleStarPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number of rows u want: ");
+		int number=sc.nextInt();
+		for(int i=1;i<=number;i++) {
+			for(int k=i;k<=number;k++)
+				System.out.print(" ");
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
+			}System.out.println();
+		}
+	}
+
+}
+
+
+/*
+
+output
+Enter a number of rows u want: 
+6
+      * 
+     * * 
+    * * * 
+   * * * * 
+  * * * * * 
+ * * * * * * 
+
+*/
